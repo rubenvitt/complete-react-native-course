49d161a09413c99ff0ee9d71c7b369165e102454
Ruben <r.vitt@fme.de>
2020-12-17 07:51:23 +0100

    :sparkles:(video-31)

--- rn-starter/src/screens/HomeScreen.js
+++ rn-starter/src/screens/HomeScreen.js
@@ -1,14 +1,31 @@
 import React from "react";
-import { Text, StyleSheet } from "react-native";
+import {Text, StyleSheet, View, Button} from "react-native";
 
-const HomeScreen = () => {
-  return <Text style={styles.text}>HomeScreen</Text>;
+const HomeScreen = ({navigation}) => {
+
+    return <View>
+        <Text style={styles.text}>Hi there!</Text>
+        <Button
+            title={'Open Components Screen'}
+            onPress={() => {
+                navigation.navigate('Components')
+            }}
+        />
+        <Button
+            title={'Open List Screen'}
+            onPress={() => navigation.navigate('List')}
+        />
+        <Button
+            title={'Open Image Screen'}
+            onPress={() => navigation.navigate('Image')}
+        />
+    </View>;
 };
 
 const styles = StyleSheet.create({
-  text: {
-    fontSize: 30
-  }
+    text: {
+        fontSize: 30
+    }
 });
 
 export default HomeScreen;
